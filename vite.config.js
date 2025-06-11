149a8badca118bcbaa7004b7329148ac8f5c5777
unknown <nickmarsh54@gmail.com>
2025-06-11 16:29:09 -0500

    more testing

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+      }
+    }
+  }
+});
